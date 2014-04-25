cab8303cf800e678c875d3ccfd23623f527c7607
Stéphane Vasseur <stephanevasseur@MacBook-Pro-de-Stephane-Vasseur.local>
2014-04-25 01:33:23 -0400

    add essentials apm to the list

--- apm-install.sh
+++ apm-install.sh
@@ -1,2 +1,15 @@
-sudo apm install editorconfig
+sudo apm install minimap
+sudo apm install minimap-color-highlight
+sudo apm install minimap-git-diff
+sudo apm install minimap-find-and-replace
 sudo apm install autocomplete-plus
+sudo apm install language-ejs
+sudo apm install langugae-ini
+sudo apm install language-sass
+sudo apm install language-less
+sudo apm install language-moustache
+sudo apm install tabs-to-spaces
+sudo apm install emmet
+sudo apm install editorconfig
+
+
