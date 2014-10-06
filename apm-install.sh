de2e111ecf115201c5effa3ff0d6c581bb38aed9
svassr <mr.vasseir@gmail.com>
2014-10-06 00:42:18 -0400

    update apm install

--- apm-install.sh
+++ apm-install.sh
@@ -1,8 +1,10 @@
+sudo apm install monokai
+sudo apm install monokai-dark
+sudo apm install monokai-seti
 sudo apm install minimap
 sudo apm install minimap-color-highlight
 sudo apm install minimap-git-diff
 sudo apm install minimap-find-and-replace
-sudo apm install autocomplete-plus
 sudo apm install language-ejs
 sudo apm install langugae-ini
 sudo apm install language-sass
@@ -11,5 +13,11 @@ sudo apm install language-moustache
 sudo apm install tabs-to-spaces
 sudo apm install emmet
 sudo apm install editorconfig
-
-
+sudo apm install Spectacular
+sudo apm install atom-beautify
+sudo apm install autocomplete-plus
+sudo apm install caniuse
+sudo apm install git-plus
+sudo apm install linter
+sudo apm install mocha
+sudo apm install seti-ui
