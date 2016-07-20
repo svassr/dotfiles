08c014d5f26c70d9fc9c1904082a9416d696b14e
Stephane Vasseur <svasseur@MacBook-Pro-de-Stephane.local>
2016-07-19 20:44:49 -0400

    update/order atom packages list

--- setup_scripts/apm-install.sh
+++ setup_scripts/apm-install.sh
@@ -1,23 +1,19 @@
-sudo apm install monokai
-sudo apm install monokai-dark
-sudo apm install monokai-seti
-sudo apm install minimap
-sudo apm install minimap-color-highlight
-sudo apm install minimap-git-diff
-sudo apm install minimap-find-and-replace
-sudo apm install language-ejs
-sudo apm install language-ini
-sudo apm install language-sass
-sudo apm install language-less
-sudo apm install language-moustache
-sudo apm install tabs-to-spaces
-sudo apm install emmet
-sudo apm install editorconfig
-sudo apm install Spectacular
-sudo apm install atom-beautify
-sudo apm install autocomplete-plus
-sudo apm install caniuse
-sudo apm install git-plus
-sudo apm install linter
-sudo apm install mocha
-sudo apm install seti-ui
+apm install atom-beautify
+# apm install autocomplete-html
+apm install editorconfig
+apm install emmet
+apm install caniuse
+apm install file-icons
+apm install git-plus
+apm install javascript-snippets
+apm install language-ejs
+apm install language-ini
+apm install language-javascript-jsx
+apm install merge-conflicts
+apm install minimap
+apm install minimap-find-and-replace
+apm install minimap-git-diff
+apm install mocha
+apm install monokai
+apm install tabs-to-spaces
+apm install webbox-color
