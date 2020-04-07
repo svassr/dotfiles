d31736bfbeeac5d15c9b05d859129aa52d592144
Stéphane Vasseur <svassr@gmail.com>
2020-04-07 09:46:24 -0400

    update app list and cask names

--- setup_scripts/macos_apps_setup.sh
+++ setup_scripts/macos_apps_setup.sh
@@ -2,19 +2,19 @@
 # OSX native apps
 
 echo "# Installing brew-cask #";
-brew install brew-cask
+brew install cask
+brew install brew-cask-completion
 
 function brewcask() {
     brew cask install "${@}" 2> /dev/null
 }
 
 # dev-related programs
-brewcask vscode
+brewcask visual-studio-code
 brew install homebrew/completions/apm-bash-completion
 brewcask intellij-idea
 brewcask dropbox
 brewcask google-chrome
-brewcask google-chrome-canary
 brewcask firefox
 brewcask opera
 brewcask iterm2
