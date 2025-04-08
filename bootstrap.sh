71498f53108d1e6235d7caafc77c47efb4e440e2
Stephane V <mr.vasseur@gmail.com>
2025-04-08 12:07:32 -0400

    Review Mathias last changes and Update doc and master branch name

--- bootstrap.sh
+++ bootstrap.sh
@@ -16,7 +16,7 @@ function doIt() {
 }
 
 echo "# dotfiles SETUP #";
-if [ "$1" == "--force" π|| "$1" == "-f" ]; then
+if [ "$1" == "--force" ] || [ "$1" == "-f" ]; then
 	doIt;
 else
 	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
